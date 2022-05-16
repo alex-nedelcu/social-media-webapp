311d313cf2e30d2b69ed9546a3b1b9d11490307a
alexnedelcu <alexandru.nedelcu01@yahoo.com>
2022-05-16 20:43:33 +0300

    Finish actions

--- a9_frontend/src/actions/authentication.js
+++ a9_frontend/src/actions/authentication.js
@@ -1,5 +1,5 @@
 import {AuthenticationService} from "../services/authentication.service";
-import {REGISTER_FAIL, REGISTER_SUCCESS, SET_MESSAGE} from "./types";
+import {LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT, REGISTER_FAIL, REGISTER_SUCCESS, SET_MESSAGE} from "./types";
 
 export const register = (username, password, role) => (dispatch) => {
     return AuthenticationService.register(username, password, role).then(
@@ -19,5 +19,29 @@ export const register = (username, password, role) => (dispatch) => {
 
             return Promise.reject();
         }
-    )
-}
+    );
+};
+
+export const login = (username, password) => (dispatch) => {
+    return AuthenticationService.login(username, password).then(
+        data => {
+            dispatch({type: LOGIN_SUCCESS, payload: {user: data}});
+            return Promise.resolve();
+        },
+        error => {
+            const message = (error.response && error.response.data && error.response.data.message)
+                || error.message
+                || error.toString();
+
+            dispatch({type: LOGIN_FAIL});
+            dispatch({type: SET_MESSAGE, payload: message});
+
+            return Promise.reject();
+        }
+    );
+};
+
+export const logout = () => (dispatch) => {
+    AuthenticationService.logout();
+    dispatch({type: LOGOUT});
+};
